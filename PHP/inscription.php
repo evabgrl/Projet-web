809688c8c58c323cf8e34b922fda038b64696c3e
evaaa <117753781+evabgrl@users.noreply.github.com>
2022-12-11 17:16:39 +0100

    Update inscription.php

--- PHP/inscription.php
+++ PHP/inscription.php
@@ -8,7 +8,7 @@
     <title>OMNES Immobilier</title>
 
 
-<body>
+<body >
     <header>
         <div style="float:left"><a href="Immobilier.php"><img src="images/logo web.png" width="150px"></a></div>
         <h1>Omnes Immobilier</h1>
@@ -34,17 +34,20 @@
         </nav>
     </nav>
     
-    <form method="post" action="creation.php">
-        Email : <input type="email" name="CourrielClient" placeholder="Entrer votre Email" class = "inscription"/><br />
-        Mdp : <input type="text" name="MotDePasse" placeholder="Entrer votre mot de passe" class = "inscription"/><br />
-        Name : <input type="text" name="NomPrenomClient" placeholder="Entrez votre nom et prénom" class = "inscription"/><br />
-        Adresse : <input type="text" name="AdresseClient" placeholder="Entrez votre adresse postale"class = "inscription"/><br />
-        Pays : <input type="text" name="PaysClient" placeholder="Entrer votre pays de résidence" class = "inscription" /><br />
-        Codepostal : <input type="text" name="CodePostalClient" placeholder="Entrer votre code postal" class = "inscription"/><br />
-        Ville : <input type="text" name="VilleClient" placeholder="Entrer votre ville de résidence" class = "inscription"/><br />
-        Numero : <input type="text" name="NumeroTelephoneClient" placeholder="Entrer votre numéro de téléphone" class = "inscription"/><br />
-        Financier : <input type="text" name="InfoFinancieres" placeholder="Entrer vos informations financières" class = "inscription"/><br />
+    <div class ="compte">
+    
+      <form method="post" action="creation.php">
+        <p class = "center">Email :</p> <input type="email" name="CourrielClient" placeholder="Entrer votre Email" /><br />
+        <p class = "center">Mdp :</p> <input type="text" name="MotDePasse" placeholder="Entrer votre mot de passe" /><br />
+        <p class = "center">Name :</p>  <input type="text" name="NomPrenomClient" placeholder="Entrez votre nom et prénom" /><br />
+        <p class = "center">Adresse :</p>  <input type="text" name="AdresseClient" placeholder="Entrez votre adresse postale"/><br />
+        <p class = "center">Pays :</p>  <input type="text" name="PaysClient" placeholder="Entrer votre pays de résidence"  /><br />
+        <p class = "center">Code :</p>  <input type="text" name="CodePostalClient" placeholder="Entrer votre code postal" /><br />
+        <p class = "center">Ville :</p>  <input type="text" name="VilleClient" placeholder="Entrer votre ville de résidence" /><br />
+        <p class = "center">Numero :</p> <input type="text" name="NumeroTelephoneClient" placeholder="Entrer votre numéro de téléphone" /><br />
+        <p class = "center">Financier :</p> <input type="text" name="InfoFinancieres" placeholder="Entrer vos informations financières" /><br />
         <input type="submit" value="Submit" class = "submit" />
-    </form>
+      </form>
+    </div>
 </body>
 </html>
